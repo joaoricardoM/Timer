c6412e78230c9c1f69cf63028536107395dea414
joaoricardoM <joao_pia_mds@hotmail.com>
2022-11-25 17:23:49 -0300

    update code

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss";
-// import { v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4} from 'uuid'
 
 class Formulario extends React.Component<{
   setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
@@ -16,11 +16,11 @@ class Formulario extends React.Component<{
     event.preventDefault();
     this.props.setTarefas((tarefasAntigas) => [
       ...tarefasAntigas,
-      { ...this.state, selecionado: false, completado: false },
+      { ...this.state, selecionado: false, completado: false, id: uuidv4() },
     ]);
     this.setState({
       tarefa: "",
-      tempo: "00:00",
+      tempo: "00:00"
     });
   }
 
